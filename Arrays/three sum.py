1365c35d9b3dac3638c479e20976067fc7960966
CHINNI ANUSHA <136144749+ChinniAnusha@users.noreply.github.com>
2025-12-27 16:04:10 +0530

    threeSum for finding zero-sum triplets
    Implement threeSum function to find triplets that sum to zero.

---
+++ Arrays/three sum.py
@@ -0,0 +1,37 @@
+#Approach: frist sort the elemnts and apply Two pointers
+#source:Leetcode,Striver a to z sheet
+#optimal solution
+
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+           n = len(nums)
+           result = []
+           nums.sort()
+           for i in range(n):
+              if i != 0 and nums[i] == nums[i-1]:
+                continue
+              j = i+1
+              k = n-1
+              while j < k:
+                sum = nums[i] + nums[j] +nums[k]
+                if sum > 0:
+                    k -= 1
+                elif sum < 0:
+                    j += 1
+                else:
+                    temp = [nums[i],nums[j],nums[k]]
+                    j += 1
+                    k -= 1
+                    result.append(temp)
+
+                    while j < k and nums[j] == nums[j-1]:
+                         j += 1
+                    while j < k and nums[k] == nums[k+1]:
+                         k -=1 
+                    
+           return result
+
+  #TC: sorting -> O(nlogn)+loops -> O(n^2) = O(n^2)​
+
+  #SC:in worst case O(n^2)
